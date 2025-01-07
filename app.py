90dc2221cfb7db6adbee33ccad21401463dec95d
David Mensah <david.mensah@hubtel.com>
2025-01-07 23:30:53 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,224 @@
+import datetime
+import os
+import uuid
+from datetime import datetime
+
+import requests
+from flask import Flask, jsonify, redirect, render_template, request, session
+from flask_cors import CORS
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from flask_session import Session
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+CORS(app)  
+
+
+# Check for environment variable 
+#set os environment variable
+os.environ["DATABASE_URL"] = "postgresql://postgres:postgres@localhost:5432/test"
+
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+
+db = scoped_session(sessionmaker(bind=engine))
+
+@app.route("/")
+def index():
+    return redirect("/login")
+
+# Login Page
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    try:
+                
+        if session.get("user_id"):
+            return render_template("index.html",message="",user_name=session.get("user_name"), user_email=session.get("user_email"), user_id=session.get("user_id"))  
+        
+        if request.method == "POST":
+            loginEmail = request.form.get("email")
+            loginPassword = request.form.get("password")
+        
+            result = db.execute(text("SELECT * FROM users WHERE email = :email AND password=:password"), {"email":loginEmail, "password":loginPassword}).fetchone()
+            
+            if result is None:
+                return render_template("login.html", message="Invalid email or password.")
+            
+            session["user_id"] = result[0]
+            session["user_email"] = result[1]
+            session["user_name"] = result[2]
+
+            return redirect("/")
+    
+        if request.method == "GET":
+            return render_template("login.html")
+        
+    except SQLAlchemyError as e:
+        print(str(e))
+  
+
+
+# Register Page
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    
+    if session.get("user_id"):
+        return redirect("/login")
+    
+    if request.method == "POST":
+        registerUsername = request.form.get("username")
+        registerEmail = request.form.get("email")
+        registerPassword = request.form.get("password")
+        userid = str(uuid.uuid4())
+
+        userquery = text("SELECT * FROM users WHERE username = :username")
+        emailquery = text("SELECT * FROM users WHERE email = :email")
+    
+        userExists = db.execute(userquery, {"username": registerUsername}).fetchone() 
+        emailExists = db.execute(emailquery, {"email": registerEmail}).fetchone() 
+    
+        if not userExists and not emailExists:
+            db.execute(text("INSERT INTO users (id,email,username, password) VALUES (:id,:email,:username, :password)"), {"id":userid,"email":registerEmail,"username":registerUsername, "password":registerPassword})
+            db.commit()
+            
+            session["user_id"] = userid
+            session["user_name"] = registerUsername
+            session["user_email"] = registerEmail
+            
+            return redirect("/")
+        
+        return render_template("signup.html", message="User with username or email already exists.")
+    if request.method == "GET":
+        return render_template("signup.html")
+
+#Home Page
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    if session.get("user_id") is None:
+        return redirect("/login")
+    
+    if request.method == "GET":
+        return render_template("index.html", user_name=session.get("user_name"))
+
+
+# Profile Page
+@app.route("/profile", methods=["GET", "POST"])
+def profile():
+    
+    if session.get("user_id") is None:
+        return redirect("/login")
+    
+    if request.method == "POST":
+        
+        profileUpdateUsername = request.form.get("username")
+        userid = session.get("user_id")
+        profileUpdatePassword = request.form.get("password")
+
+        userquery = text("SELECT * FROM users WHERE id = :id")
+        
+        userExists = db.execute(userquery, {"id":userid}).fetchone()
+        
+        if userExists:
+            
+            if userExists[2] == profileUpdateUsername:
+                profileUpdateUsername = userExists[2]
+            else:
+                profileUpdateUsername = profileUpdateUsername
+            
+            if profileUpdatePassword is not None and profileUpdatePassword != "":
+                profileUpdatePassword = profileUpdatePassword
+            else:
+                profileUpdatePassword = userExists[3]
+                
+            db.execute(text("UPDATE users SET username = :username, password = :password WHERE id = :id"), {"username": profileUpdateUsername,"password":profileUpdatePassword, "id": userid})
+            db.commit()
+            
+            session["user_id"] = userid
+            session["user_name"] = profileUpdateUsername
+            
+            return redirect("/home")
+        
+        return render_template("profile.html", message="User with username already exists.", user_name=session.get("user_name"), user_email=session.get("user_email"), user_id=session.get("user_id"))
+        
+    if request.method == "GET":
+        return render_template("profile.html", user_name=session.get("user_name"), user_email=session.get("user_email"), user_id=session.get("user_id"))
+
+
+
+@app.route('/api/police-stations', methods=['GET'])
+def get_police_stations_data():
+    # Forward request to the actual API
+    base_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/9aeefa17-27e8-4dd9-b74d-80f7f9eb85ac/resource/c0176e24-8b76-4bb2-96fa-61cc1af2a065/download/police-facility-locations-4326.geojson"
+    url = f"{base_url}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return jsonify(response.json())  # Return API data as JSON
+    except requests.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+
+
+@app.route('/api/vehicle-charging-stations', methods=['GET'])
+def get_vehicle_charging_stations_data():
+    # Forward request to the actual API
+    base_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/city-operated-electric-vehicle-charging-station-map/resource/dcd8f6fc-dd41-4f9d-a4d8-dd4ef19915fd/download/city-operated-electric-vehicle-charging-station-map-4326.geojson"
+    url = f"{base_url}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return jsonify(response.json())  # Return API data as JSON
+    except requests.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
+
+
+@app.route('/api/cafeto-data', methods=['GET'])
+def get_cafeto_data():
+    # Forward request to the actual API
+    base_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/3b605a2e-f3bf-4b2b-b972-c0829b2788f5/resource/aa839e97-df7f-4c65-8aba-d336bb3c8f06/download/cafe-to-locations-4326.geojson"
+    url = f"{base_url}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return jsonify(response.json())  # Return API data as JSON
+    except requests.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+    
+
+
+@app.route('/api/air-conditioned-places', methods=['GET'])
+def get_air_conditioned_data():
+    # Forward request to the actual API
+    base_url = "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/4aab06c4-9689-4e2c-9476-18ffee312640/resource/f0d66bc3-16cb-4491-97af-0fae10913666/download/air-conditioned-and-cool-spaces-4326.geojson"
+    url = f"{base_url}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return jsonify(response.json())  # Return API data as JSON
+    except requests.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
+
+# Logout Page
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/map.js
@@ -0,0 +1,510 @@
+// Document ready function
+$(document).ready(function () {
+  // Set the current date
+  document.getElementById("currentDate").innerHTML = new Date().toUTCString();
+
+  let userLat = 0;
+  let userLon = 0;
+
+  const selectedChargingStations = L.markerClusterGroup();
+  let selectedChargingStationsIsInView = false;
+
+  //
+  // Leaflet Js Implementations
+  //
+  var map = L.map("map").setView([43.698327247001203, -79.436683017408996], 13);
+
+  // Add the tile layer
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    maxZoom: 18,
+  }).addTo(map);
+
+  // Add a scale to the map
+  L.control.scale({ position: "bottomleft" }).addTo(map);
+
+  // Add a blue bootstrap badge
+  var badge = L.control({ position: "topright" });
+
+  badge.onAdd = function (map) {
+    var div = L.DomUtil.create("div", "badge badge-primary p-2");
+    div.innerHTML = "Engineered By: David Edem Mensah";
+    return div;
+  };
+
+  setTimeout(function () {
+    badge.addTo(map);
+  }, 5000);
+
+  // Refresh map
+
+  // Add a blue bootstrap badge
+  var refreshBadge = L.control({ position: "bottomright" });
+
+  refreshBadge.onAdd = function (map) {
+    var refresher = L.DomUtil.create(
+      "div",
+      "badge badge-danger p-2 refresh-badge"
+    );
+    refresher.innerHTML = "Refresh to view all map features";
+    refresher.addEventListener("click", function () {
+      map.eachLayer(function (layer) {
+        if (
+          layer instanceof L.Marker ||
+          layer instanceof L.MarkerClusterGroup
+        ) {
+          map.removeLayer(layer);
+        }
+      });
+
+      policeStationLayer.addTo(map);
+      vehicleChargingStationLayer.addTo(map);
+      cafeToLayer.addTo(map);
+      coolPlacesStationLayer.addTo(map);
+    });
+    return refresher;
+  };
+
+  setTimeout(function () {
+    refreshBadge.addTo(map);
+  }, 5000);
+
+  //
+
+  let policeStationCount = 0;
+  let vehicleChargingStationCount = 0;
+  let coolPlacesCount = 0;
+  let cafeToCount = 0;
+
+  // Add map features
+  //
+
+  /*
+    Add the police stations layer
+  */
+
+  const policeStationLayer = L.markerClusterGroup();
+
+  L.geoJSON.ajax("http://127.0.0.1:5000/api/police-stations", {
+    onEachFeature: function (feature) {
+      policeStationCount++;
+    },
+    pointToLayer: function (feature, latlng) {
+      let marker = L.circleMarker(latlng, {
+        radius: 5,
+        fillColor: "blue",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
+      });
+      let policeStationPopupContent = `<span>Organization:</span> <strong>${feature.properties.ORGANIZATION}</strong></br>
+      <span>Facility:</span> <strong>${feature.properties.FACILITY}</strong></br>
+      <span>Postal Code:</span> <strong>${feature.properties.POSTAL_CODE}</strong></br>
+      <span>Address:</span> <strong>${feature.properties.ADDRESS}</strong></br>`;
+      marker.bindPopup(policeStationPopupContent);
+      policeStationLayer.addLayer(marker);
+      return marker;
+    },
+  });
+
+  /*
+    End of adding the police stations layer
+  */
+
+  /*
+    Add the City vehicle charging stations layer
+  */
+  const vehicleChargingStationLayer = L.markerClusterGroup();
+
+  L.geoJSON.ajax("http://127.0.0.1:5000/api/vehicle-charging-stations", {
+    pointToLayer: function (feature, latlng) {
+      let marker = L.marker(latlng, {
+        icon: L.icon({
+          iconUrl: "static/image/thnb.webp",
+          iconSize: [20, 24],
+          iconAnchor: [12, 41],
+          popupAnchor: [1, -34],
+          shadowSize: [41, 41],
+        }),
+      });
+
+      let vehicleChargingStationPopupContent = `<span>Address:</span> <strong>${feature.properties.Address}</strong></br>
+      <span>Location:</span> <strong>${feature.properties.Location}</strong></br>
+      <span>Type:</span> <strong>${feature.properties.Type}</strong></br>
+      <span>Level 2 Charging Ports:</span> <strong>${feature.properties["Level3_Charging_Ports"]}</strong></br>
+      <span>Level 3 Charging Ports:</span> <strong>${feature.properties["Level2_Charging_Ports"]}</strong></br>`;
+
+      marker.bindPopup(vehicleChargingStationPopupContent);
+
+      vehicleChargingStationLayer.addLayer(marker);
+
+      return marker;
+    },
+    onEachFeature: function (feature, layer) {
+      vehicleChargingStationCount++;
+    },
+  });
+
+  /*
+    End of the City vehicle charging stations layer
+  */
+
+  /*
+    Add cafeto locations
+  */
+
+  const cafeToLayer = L.markerClusterGroup();
+
+  L.geoJSON.ajax("http://127.0.0.1:5000/api/cafeto-data", {
+    pointToLayer: function (feature, latlng) {
+      let marker = L.circleMarker(latlng, {
+        radius: 5,
+        fillColor: "green",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
+      });
+
+      cafeToLayer.addLayer(marker);
+
+      return marker;
+    },
+    onEachFeature: function (feature, layer) {
+      cafeToCount++;
+    },
+  });
+
+  /*
+    End of add cafeto locations
+  */
+
+  /*
+    Add the air conditioned and cool places stations layer
+  */
+  const coolPlacesStationLayer = L.markerClusterGroup();
+
+  L.geoJSON.ajax("http://127.0.0.1:5000/api/air-conditioned-places", {
+    pointToLayer: function (feature, latlng) {
+      let marker = L.circleMarker(latlng, {
+        radius: 5,
+        fillColor: "#f72b26",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
+      });
+
+      let coolPlacesStationPopupContent = `<span>Address:</span> <strong>${feature.properties.address}</strong></br>
+        <span>Location:</span> <strong>${feature.properties.locationName}</strong></br>
+        <span>Description:</span> <strong>${feature.properties.locationTypeCode}</strong></br>
+        <span>Phone:</span> <strong>${feature.properties.phone}</strong></br>
+        <span>Eligibility:</span> <strong>${feature.properties.eligibility}</strong></br>`;
+
+      marker.bindPopup(coolPlacesStationPopupContent);
+
+      coolPlacesStationLayer.addLayer(marker);
+
+      return marker;
+    },
+    onEachFeature: function (feature, layer) {
+      coolPlacesCount++;
+    },
+  });
+
+  /*
+    End of adding the air conditioned and cool places stations layer
+    */
+
+  /*
+  Get the current location of the user
+  */
+
+  // Function to get user's location coordinates
+
+  let showShareLocCordinates = document.getElementById(
+    "showShareLocCordinates"
+  );
+
+  function getUserLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        userLat = position.coords.latitude;
+        userLon = position.coords.longitude;
+        var coordinates = "Latitude: " + userLat + ", Longitude: " + userLon;
+        showShareLocCordinates.innerHTML = coordinates;
+
+        map.flyTo([userLat, userLon], 13, {
+          duration: 2,
+          easeLinearity: 0.25,
+        }); // Center the map to user's location
+
+        L.marker([position.coords.latitude, position.coords.longitude])
+          .addTo(map)
+          .bindPopup("You are here!")
+          .openPopup();
+      });
+
+      isLocationShared = true;
+
+      shareLocBtn.disabled = true;
+    } else {
+      showShareLocCordinates.innerHTML =
+        "Geolocation is not supported by this browser.";
+    }
+  }
+
+  let shareLocBtn = document.getElementById("shareLocBtn");
+  shareLocBtn.addEventListener("click", getUserLocation);
+
+  /*
+  End of getting the current location of the user
+  */
+
+  // add base map layers
+  const baseLayers = {
+    "Police Stations": policeStationLayer,
+    // "Vehicle Charging Stations": vehicleChargingStationLayer,
+    "Cafeto Locations": cafeToLayer,
+    "Cool Places": coolPlacesStationLayer,
+  };
+
+  // add layer control to the map
+  setTimeout(function () {
+    L.control.layers(baseLayers).addTo(map);
+  }, 6000);
+
+  // Add a map Legend
+  var legend = L.control({ position: "bottomright" });
+
+  legend.onAdd = function (map) {
+    var div = L.DomUtil.create("div", "legend");
+
+    div.innerHTML = `<div class="mapLegend"><h5>Legend</h5>
+                <i style="background: #2d2ef3"></i> Police Stations <small class="badge badge-police"><span id="">*</span></small><br>
+                <i style="background: #ffa500"></i> Vehicle Charging Stations <small class="badge badge-vehicle-charge"><span id="">*</span></small><br> 
+                <i style="background: #f72b26"></i> Cool Places <small class="badge badge-cool-places"><span id="">*</span></small><br>
+                <i style="background: #197f19"></i> CafeTo Restaurants <small class="badge badge-cafeto"><span id="">*</span></small></div><br>`;
+
+    return div;
+  };
+
+  setTimeout(function () {
+    legend.addTo(map);
+  }, 4000);
+
+  /*
+  Toggle map layers
+  */
+  let showAllLayers = true;
+
+  let viewForLayers = document.getElementById("viewForLayers");
+
+  function ToggleMapLayers() {
+    if (showAllLayers) {
+      policeStationLayer.addTo(map);
+      if (!selectedChargingStationsIsInView) {
+        vehicleChargingStationLayer.addTo(map);
+      }
+      cafeToLayer.addTo(map);
+      coolPlacesStationLayer.addTo(map);
+
+      viewForLayers.style.display = "block";
+      showAllLayers = false;
+    } else {
+      map.removeLayer(policeStationLayer);
+      map.removeLayer(vehicleChargingStationLayer);
+      map.removeLayer(cafeToLayer);
+      map.removeLayer(coolPlacesStationLayer);
+
+      showAllLayers = true;
+      viewForLayers.style.display = "none";
+    }
+  }
+
+  let viewAllLayersId = document.getElementById("viewAllLayersId");
+  if (viewAllLayersId?.length && viewForLayers?.length) {
+    viewAllLayersId?.addEventListener("click", ToggleMapLayers);
+
+    setTimeout(function () {
+      document.getElementById(
+        "police-station-count"
+      ).innerHTML = `Police Stations: ${policeStationCount}`;
+      document.getElementById(
+        "vehicle-charge-count"
+      ).innerHTML = `Vehicle Charging Station: ${vehicleChargingStationCount}`;
+      document.getElementById(
+        "cools-places-count"
+      ).innerHTML = `Cool Places: ${coolPlacesCount}`;
+      document.getElementById(
+        "cafeto-count"
+      ).innerHTML = `CafeTo Locations: ${cafeToCount}`;
+      viewForLayers.style.display = "block";
+      ToggleMapLayers();
+    }, 4000);
+  }
+
+  /*
+  End of toggle map layers
+  */
+
+  /*
+  Get the nearest vehicle charging station
+  */
+
+  let getNearestShortTermRentalsBtn = document.getElementById(
+    "getNearestShortTermRentalsBtn"
+  );
+
+  let numberOfLocations = document.getElementById("numberOfLocations");
+
+  let maximumVehicleChargingStationNumber = 0;
+
+  numberOfLocations.value = 1;
+
+  getNearestShortTermRentalsBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    maximumVehicleChargingStationNumber = parseInt(numberOfLocations.value);
+
+    if (userLat == 0 && userLon == 0) {
+      alert("Please share your location first");
+      return;
+    }
+
+    if (
+      maximumVehicleChargingStationNumber <= 0 ||
+      maximumVehicleChargingStationNumber > 100
+    ) {
+      alert("Please enter a valid number from 1 to 100");
+      return;
+    }
+
+    getNearestVehicleChargingStation(map, selectedChargingStations);
+  });
+
+  function getNearestVehicleChargingStation(map, selectedChargingStations) {
+    // Clear existing layers
+    map.eachLayer(function (layer) {
+      if (layer instanceof L.Marker || layer instanceof L.MarkerClusterGroup) {
+        map.removeLayer(layer);
+      }
+    });
+
+    if (selectedChargingStationsIsInView) {
+      map.removeLayer(selectedChargingStations);
+    }
+
+    // Fetch GeoJSON data and process
+    fetch("http://127.0.0.1:5000/api/vehicle-charging-stations")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            `Network response was not ok: ${response.statusText}`
+          );
+        }
+        return response.json();
+      })
+      .then((geojson) => {
+        console.log("GeoJSON data fetched:", geojson);
+
+        // Calculate distances and add to features
+        const userLocation = [userLon, userLat];
+        geojson.features.forEach((feature) => {
+          const distance = turf.distance(
+            turf.point(userLocation),
+            turf.point(feature.geometry.coordinates[0]),
+            { units: "kilometers" }
+          );
+          feature.properties.distance = distance;
+        });
+
+        // Sort and filter the nearest stations
+        const sortedStations = geojson.features
+          .sort((a, b) => a.properties.distance - b.properties.distance)
+          .slice(0, maximumVehicleChargingStationNumber);
+
+        console.log(
+          `Displaying ${sortedStations.length} nearest vehicle charging stations`
+        );
+
+        // Add only the sorted and filtered stations to the cluster group
+        sortedStations.forEach((station) => {
+          const latlng = [
+            station.geometry.coordinates[0][1], // Latitude
+            station.geometry.coordinates[0][0], // Longitude
+          ];
+
+          const marker = L.marker(latlng, {
+            icon: L.icon({
+              iconUrl: "static/image/thnb2.png",
+              iconSize: [20, 24],
+              iconAnchor: [12, 41],
+              popupAnchor: [1, -34],
+              shadowSize: [41, 41],
+            }),
+          });
+
+          const popupContent = `
+            <span>Address:</span> <strong>${
+              station.properties.Address
+            }</strong><br>
+            <span>Location:</span> <strong>${
+              station.properties.Location
+            }</strong><br>
+            <span>Type:</span> <strong>${station.properties.Type}</strong><br>
+            <span>Level 2 Charging Ports:</span> <strong>${
+              station.properties["Level2_Charging_Ports"]
+            }</strong><br>
+            <span>Level 3 Charging Ports:</span> <strong>${
+              station.properties["Level3_Charging_Ports"]
+            }</strong><br>
+            <span>Distance:</span> <strong>${station.properties.distance.toFixed(
+              2
+            )} km</strong><br>
+          `;
+
+          marker.bindPopup(popupContent);
+          selectedChargingStations.addLayer(marker);
+        });
+
+        // Add the filtered cluster group to the map
+        map.addLayer(selectedChargingStations);
+
+        const bounds = selectedChargingStations.getBounds();
+        map.flyToBounds(bounds, {
+          maxZoom: 16,
+          padding: [50, 50],
+          duration: 2,
+          easeLinearity: 0.25,
+        });
+
+        selectedChargingStationsIsInView = true;
+
+        setTimeout(function () {
+          policeStationLayer.addTo(map);
+          cafeToLayer.addTo(map);
+          coolPlacesStationLayer.addTo(map);
+        }, 5000);
+
+        // Debug: Final cluster group layer count
+        console.log(
+          "Total markers added to cluster group:",
+          selectedChargingStations.getLayers().length
+        );
+      })
+      .catch((error) => {
+        console.error("Error fetching or processing GeoJSON data:", error);
+      });
+  }
+
+  /*
+  End of get the nearest vehicle charging station
+  */
+
+  //
+  // End of map features
+  //
+});
